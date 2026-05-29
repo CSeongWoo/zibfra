cef67a361dbd9769d8fba9c43c9ef6c499b0ca72
CSeongWoo <greentjddn@gmail.com>
2026-05-29 14:06:57 +0900

    first commit

---
+++ src/main/java/com/example/zipfra/ZipfraApplication.java
@@ -0,0 +1,13 @@
+package com.example.zipfra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipfraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipfraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zipfra/ZipfraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zipfra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipfraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
