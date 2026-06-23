ef304528a0fc9a5a353f8f6c72af4f625bb5f44f
kw <rimblee2000@naver.com>
2026-06-23 09:51:18 +0900

    401버그 수정

--- zipfra_frontend/src/api/favorite.js
+++ zipfra_frontend/src/api/favorite.js
@@ -1,11 +1,11 @@
 import http from './http';
 
 export async function toggleFavorite(propertyId) {
-  const response = await http.post(`/api/v1/favorites/${propertyId}`);
+  const response = await http.post(`/favorites/${propertyId}`);
   return response.data;
 }
 
 export async function getFavorites() {
-  const response = await http.get('/api/v1/favorites');
+  const response = await http.get('/favorites');
   return response.data;
 }
